6ea838b25d5b5023ae4b4052fc2a8e015b4e49a7
zhongchongbin <405271667@qq.com>
2017-10-25 17:47:54 +0800

    返回toolbar的Activity基类

---
+++ app/src/main/java/com/zcbin/xnews/base/BaseBackActivity.java
@@ -0,0 +1,40 @@
+package com.zcbin.xnews.base;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.zcbin.xnews.R;
+
+/**
+ * Created by zcbin on 2017/10/25.
+ * 返回toolbar的Activity
+ */
+
+public abstract class BaseBackActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    protected void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar == null) return;
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
